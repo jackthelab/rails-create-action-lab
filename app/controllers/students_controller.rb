639466a4254af92c579b4ccfbde4ce1b0149fa24
Jacob Hampton <jhampton792@gmail.com>
2021-02-08 10:11:38 -0600

    commit tree b9843ee99e83f5dbf6987a0478d3856c7980374e

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,16 @@ class StudentsController < ApplicationController
   end
 
   def create
+    # @student = Student.new(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+
+    params.require(:student).permit(:first_name, :last_name)
+    @student = Student.name(params)
+
+    #
+
+    if @student.save
+      redirect_to student_path(@student)
+    end
   end
 
 end
